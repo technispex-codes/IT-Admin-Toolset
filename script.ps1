85a563bbb1f901be5001522c17b8896b5fb63c61
Alexx <106840481+technispex-codes@users.noreply.github.com>
2024-05-22 11:18:10 -0400

    Update script.ps1
    fixed syntactical errors

--- script.ps1
+++ script.ps1
@@ -5,23 +5,23 @@ New-Item -ItemType Directory -Path C:\working-temp
 Write-Host "Installing Sysinternals..."
 
 ## Sysinternals
-Invoke-WebRequest -Uri "https://download.sysinternals.com/files/SysinternalsSuite.zip" -Outfile "C:\working-temp\SysinternalsSuite.zip" -Verbose
+wget -Uri "https://download.sysinternals.com/files/SysinternalsSuite.zip" -Outfile "C:\working-temp\SysinternalsSuite.zip" -Verbose
 Expand-Archive -Path "C:\working-temp\SysinternalsSuite.zip" -Verbose -DestinationPath "C:\Users\Public\Desktop\Sysinternals"
 
 ## VS Code
 Write-Host "Installing VS Code..."
-Invoke-WebRequest -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64" -Outfile "C:\working-temp\VSCodeSetup.exe"
-Start-Process "C:\working-temp\VSCodeSetup.exe" -Arguments "/VERYSILENT /MERGETASKS=!runcode"
+wget -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64" -Outfile "C:\working-temp\VSCodeSetup.exe"
+Start-Process "C:\working-temp\VSCodeSetup.exe" -Argument "/VERYSILENT /MERGETASKS=!runcode"
 
 # Create a shortcut for VS Code
-$WshShell = New-Object -comObject WWScript.Shell
+$WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Visual Studio Code.lnk")
 $Shortcut.TargetPath = "C:\Program Files\Microsoft VS Code\Code.exe"
 $Shortcut.Save()
 
 ## Wireshark
 Write-Host "Installing Wireshark..."
-Invoke-WebRequest -Uri "https://2.na.dl.wireshark.org/win64/Wireshark-4.2.5-x64.exe" -OutFile "C:\working-temp\WiresharkInstaller.exe"
+wget -Uri "https://2.na.dl.wireshark.org/win64/Wireshark-4.2.5-x64.exe" -OutFile "C:\working-temp\WiresharkInstaller.exe"
 Start-Process "C:\working-temp\WiresharkInstaller.exe" -Argument "/S /desktopicon=YES"
 
 ## Clean up
